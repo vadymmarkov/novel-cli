71619db7c8c205acfa54ac391a2d6d538732556e
Vadym Markov <markov.vadym@gmail.com>
2016-12-20 00:24:31 +0100

    Implement basic script

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
   name: "NovelCLI",
   dependencies: [
     .Package(url: "https://github.com/kylef/Commander", majorVersion: 0, minor: 6),
+    .Package(url: "https://github.com/kareman/SwiftShell.git", "3.0.0-beta")
   ]
 )
