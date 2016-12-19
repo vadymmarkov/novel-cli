64196290b67b1a2091d409ff70966cdd1ea2ceb0
Vadym Markov <markov.vadym@gmail.com>
2016-12-20 00:38:53 +0100

    Use chdir

--- Sources/NovelCLI/main.swift
+++ Sources/NovelCLI/main.swift
@@ -3,18 +3,20 @@ import SwiftShell
 
 struct AssetTask {
 
-  func execute() throws {
-    try runAndPrint(bash: "mkdir -p Public/css")
-    try runAndPrint(bash: "mkdir -p Public/fonts")
-    try runAndPrint(bash: "mkdir -p Public/images")
-    try runAndPrint(bash: "mkdir -p Public/js")
+  let root: String
 
-    try runAndPrint(bash: "cp Packages/Novel-*/Public/css/vendor.css Public/css")
-    try runAndPrint(bash: "cp Packages/Novel-*/Public/css/admin.css Public/css")
-    try runAndPrint(bash: "cp -a Packages/Novel-*/Public/fonts/. Public/fonts")
-    try runAndPrint(bash: "cp -a Packages/Novel-*/Public/images/. Public/images")
-    try runAndPrint(bash: "cp Packages/Novel-*/Public/js/vendor.css Public/js")
-    try runAndPrint(bash: "cp Packages/Novel-*/Public/js/admin.css Public/js")
+  func execute() throws {
+    try runAndPrint(bash: "mkdir -p \(root)/Public/css")
+    try runAndPrint(bash: "mkdir -p \(root)/Public/fonts")
+    try runAndPrint(bash: "mkdir -p \(root)/Public/images")
+    try runAndPrint(bash: "mkdir -p \(root)/Public/js")
+
+    try runAndPrint(bash: "cp \(root)/Packages/Novel-*/Public/css/vendor.css \(root)/Public/css")
+    try runAndPrint(bash: "cp \(root)/Packages/Novel-*/Public/css/admin.css \(root)/Public/css")
+    try runAndPrint(bash: "cp -a \(root)/Packages/Novel-*/Public/fonts/. \(root)/Public/fonts")
+    try runAndPrint(bash: "cp -a \(root)/Packages/Novel-*/Public/images/. \(root)/Public/images")
+    try runAndPrint(bash: "cp \(root)/Packages/Novel-*/Public/js/vendor.css \(root)/Public/js")
+    try runAndPrint(bash: "cp \(root)/Packages/Novel-*/Public/js/admin.css \(root)/Public/js")
   }
 }
 
@@ -22,14 +24,13 @@ Group {
   $0.command("new") { (name: String) in
     print("Cloning template...")
     try runAndPrint(bash: "git clone https://github.com/vadymmarkov/novel-template \(name)")
-    try runAndPrint(bash: "cd \(name)")
-    try runAndPrint(bash: "swift build")
-    try AssetTask().execute()
+    try runAndPrint(bash: "swift build --chdir \(name)")
+    try AssetTask(root: name).execute()
   }
 
   $0.command("update") {
     print("Updating Novel")
     try runAndPrint(bash: "swift package update")
-    try AssetTask().execute()
+    try AssetTask(root: ".").execute()
   }
 }.run()
